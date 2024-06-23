7ab09ca6c9dfad197a46bb5fe2c3dc9422db504f
ian_mendes202 <ian.luca202@gmail.com>
2024-06-23 16:41:24 -0300

    2024-06-23 16:41:24

--- src/modules/sql-utils.ts
+++
@@ -1,8 +0,0 @@
-import { QueryResultRow, sql } from "@vercel/postgres";
-const MangoDB = {
-    async query ( query: string ): Promise<QueryResultRow[]> {
-        var { rows } = await sql`${query}`;
-        return rows;
-    }
-}
-export default MangoDB;
